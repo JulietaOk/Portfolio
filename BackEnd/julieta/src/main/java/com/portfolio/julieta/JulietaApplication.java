0b66d188ed84056a3c170c5bd7b0ca94db36ef52
JulietaOk <julieta.lopez@live.com>
2022-07-06 18:57:26 -0300

    BackEnd

---
+++ BackEnd/julieta/src/main/java/com/portfolio/julieta/JulietaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.julieta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulietaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulietaApplication.class, args);
+	}
+
+}
