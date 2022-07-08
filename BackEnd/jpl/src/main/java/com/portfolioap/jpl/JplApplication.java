63788db981c2aef8c2c0f9a65693e3e186e5c5e3
JulietaOk <julieta.lopez@live.com>
2022-07-08 10:32:13 -0300

    BackEnd

---
+++ BackEnd/jpl/src/main/java/com/portfolioap/jpl/JplApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioap.jpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JplApplication.class, args);
+	}
+
+}
---
+++ BackEnd/jpl/src/test/java/com/portfolioap/jpl/JplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioap.jpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
